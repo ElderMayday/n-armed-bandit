26d80719a607a22a6fb73a447f4591a59c967307
ElderMayday <galdarsar@gmail.com>
2016-12-06 23:34:02 +0100

    Add RandomNormal

---
+++ nArmedBandit/Backend/RandomNormal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace nArmedBandit.Backend
+{
+    class RandomNormal
+    {
+        public double Mean { get; protected set; }
+        public double Deviation { get; protected set; }
+        protected int rounds;
+        protected static Random random = new Random();
+
+        public RandomNormal(double mean, double deviation)
+        {
+            this.Mean = mean;
+            this.Deviation = deviation;
+            rounds = 12;
+        }
+
+        public double Generate()
+        {
+            double result = 0.0;
+
+            for (int i = 0; i < rounds; i++)
+                result += random.NextDouble();
+
+            return Deviation * Math.Sqrt(12.0 / rounds) * (result - rounds / 2.0) + Mean;
+        }
+    }
+}
